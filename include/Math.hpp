41dea0b37a7c285866d444526d82fb3592102768
Renato Cordeiro Ferreira <renato.cferreira@hotmail.com>
2015-05-21 11:19:17 -0300

    Use GMP for multiprecision calculation

--- include/Math.hpp
+++ include/Math.hpp
@@ -1,6 +1,15 @@
 #ifndef HPP_MATH_DEFINED
 #define HPP_MATH_DEFINED
 
-double calculateCosine(double radians);
+// GMP headers
+#include <gmpxx.h>
+
+// Alias
+using mpz = mpz_class;
+using mpf = mpf_class;
+
+mpz factorial(mpz n);
+
+mpf calculateCosine(mpf radians);
 
 #endif
--- src/Math.cpp
+++ src/Math.cpp
@@ -2,17 +2,13 @@
 #include <cmath>
 #include <iostream>
 
-// Libraries
+// Local headers
 #include "Math.hpp"
 
 const double PI = 3.141592653589793238462643;
 
-// Alias
-using mpz = unsigned long int;
-using mpf = double;
-
-mpz factorial (mpz n) {
-  return (n == 1 || n == 0) ? 1 : factorial(n - 1) * n;
+mpz factorial(mpz n) {
+  return (n == 1 || n == 0) ? mpz(1) : factorial(n - 1) * n;
 }
 
 mpf fixRadians_impl(mpf radians) {
@@ -22,16 +18,19 @@ mpf fixRadians_impl(mpf radians) {
 }
 
 mpf fixRadians(mpf radians) {
-  return fixRadians_impl(std::abs(radians));
+  return fixRadians_impl(abs(radians));
 }
 
 mpf calculateCosine(mpf radians) {
   mpf cos = 0;
+  mpf aux = 0;
+
   radians = fixRadians(radians);
 
-  for (int n = 0; n < 10; n++) {
+  for (int n = 0; n < 100; n++) {
     int signal = n % 2 == 0 ? 1 : -1;
-    cos += signal * (std::pow(radians, 2*n) / factorial(2*n));
+    mpf_pow_ui(aux.get_mpf_t(), radians.get_mpf_t(), 2*n);
+    cos += signal * (aux / factorial(2*n));
   }
 
   return cos;
