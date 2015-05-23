3a080c0a99bc9a2060cc0ae1940966c5bd795294
Renato Cordeiro Ferreira <renato.cferreira@hotmail.com>
2015-05-23 20:23:57 -0300

    Extract function to calculate cosine

--- src/Math.cpp
+++ src/Math.cpp
@@ -21,17 +21,19 @@ mpf fixRadians(const mpf& radians) {
   return fixRadians_impl(abs(radians));
 }
 
+mpf calculateTerm(const mpf& radians, unsigned int n) {
+  mpf aux = 0; int signal = n % 2 == 0 ? 1 : -1;
+  mpf_pow_ui(aux.get_mpf_t(), radians.get_mpf_t(), 2*n);
+  return signal * (aux / factorial(2*n));
+}
+
 mpf singleThreadedCosine(const mpf& radians) {
-  mpf cos = 0;
-  mpf aux = 0;
+  mpf cos = 0, aux = 0;
 
   mpf fixed_radians { fixRadians(radians) };
 
-  for (int n = 0; n < 100; n++) {
-    int signal = n % 2 == 0 ? 1 : -1;
-    mpf_pow_ui(aux.get_mpf_t(), fixed_radians.get_mpf_t(), 2*n);
-    cos += signal * (aux / factorial(2*n));
-  }
+  for (unsigned int n = 0; n < 100; n++)
+    cos += calculateTerm(radians, n);
 
   return cos;
 }
